90c7d56700b241f104e98a889b29ce197dbea941
Sera Programmer <73139993+seraprogrammer@users.noreply.github.com>
2024-06-04 17:53:07 +0600

    Update index.js

--- index.js
+++ index.js
@@ -26,25 +26,6 @@ const generationConfig = {
   responseMimeType: "text/plain",
 };
 
-const safetySettings = [
-  {
-    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  },
-];
-
 async function getAIResponse(input, mentionedUser, retries = 1) {
   try {
     const chatSession = await model.startChat({
